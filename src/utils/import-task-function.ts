ef627adf3e1a0dd88bc89c4ac92e60960ef544f8
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-05-10 03:41:22 +0000

    chore: remove unused lines

--- src/utils/import-task-function.ts
+++ src/utils/import-task-function.ts
@@ -1,5 +1,5 @@
 import { TaskFunction } from '@src/types.js'
-import { isFunction, isObject, isUndefined } from '@blackglory/prelude'
+import { isFunction, isObject } from '@blackglory/prelude'
 
 export async function importTaskFunction<Result, Params>(
   filename: string
@@ -7,31 +7,19 @@ export async function importTaskFunction<Result, Params>(
   const module = await import(filename)
 
   // module.exports = function () {}
-  if (
-    (isFunction(module.init) || isUndefined(module.init)) &&
-    (isFunction(module.final) || isUndefined(module.final)) &&
-    isFunction(module)
-  ) {
+  if (isFunction(module)) {
     return module
   }
 
   // export default function() {}
-  if (
-    (isFunction(module.init) || isUndefined(module.init)) &&
-    (isFunction(module.final) || isUndefined(module.final)) &&
-    isObject(module) && isFunction(module.default)
-  ) {
+  if (isObject(module) && isFunction(module.default)) {
     return module.default
   }
 
   // exports.default = function() {}
-  if (
-    (isFunction(module.default.init) || isUndefined(module.default.init)) &&
-    (isFunction(module.default.final) || isUndefined(module.default.final)) &&
-    isObject(module) && isObject(module.default) && isFunction(module.default.default)
-  ) {
+  if (isObject(module) && isObject(module.default) && isFunction(module.default.default)) {
     return module.default.default
   }
 
-  throw new Error('Invalid task module')
+  throw new Error('Invalid task function')
 }
